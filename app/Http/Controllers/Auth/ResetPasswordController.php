70f853f31c43a29e304c3251a92908c6c70f2b6c
liushiqi <liushiqi1@100tal.com>
2019-12-16 15:01:24 +0800

    找回密码成功消息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -40,6 +41,7 @@ class ResetPasswordController extends Controller
     protected function sendResetResponse(Request $request, $response)
     {
         session()->flash('success', '密码更新成功，您已成功登录！');
+
         return redirect($this->redirectPath());
     }
 }
