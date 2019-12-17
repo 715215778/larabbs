d2fa325f4dca1afc0c385fba063450502a67edce
liushiqi <liushiqi1@100tal.com>
2019-12-17 10:42:17 +0800

    用户可以上传话题图片

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -68,7 +68,6 @@
 
 
 
-
 @section('scripts')
     <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
@@ -79,6 +78,16 @@
         $(document).ready(function() {
             var editor = new Simditor({
                 textarea: $('#editor'),
+                upload: {
+                    url: '{{ route('topics.upload_image') }}',
+                    params: {
+                        _token: '{{ csrf_token() }}'
+                    },
+                    fileKey: 'upload_file',
+                    connectionCount: 3,
+                    leaveConfirm: '文件上传中，关闭此页面将取消上传。'
+                },
+                pasteImage: true,
             });
         });
     </script>
